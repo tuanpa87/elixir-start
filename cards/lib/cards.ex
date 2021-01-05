ac186698ab99adbdd922db227ee23c4ee9e1b0ab
tuanpa87 <tuanpa87@users.noreply.github.com>
2021-01-05 22:53:26 +0700

    Searching a List

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -2,8 +2,10 @@ defmodule Cards do
     def create_deck do
         ["Ace", "Two", "Three"]
     end
-
     def shuffle(deck) do
         Enum.shuffle(deck)
+    end
+    def contains?(deck, card) do
+        Enum.member?(deck, card)
     end
 end
