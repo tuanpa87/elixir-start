72af2058c51ae9bf76b2ce942ec6d79cc0c58e8b
tuanpa87 <tuanpa87@users.noreply.github.com>
2021-01-06 00:09:58 +0700

    Solving Nested Arrays 2

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -3,13 +3,9 @@ defmodule Cards do
         values = ["Ace", "Two", "Three", "Four", "Five"]
         suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
 
-        cards = for value <- values do
-            for suit <- suits do
-                "#{value} of #{suit}"
-            end
-        end
-
-		List.flatten(cards)
+        for suit <- suits, value <- values do
+			"#{value} of #{suit}"
+		end
     end
 
     def shuffle(deck) do
