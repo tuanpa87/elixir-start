4ea533dc70ee09daf4fb23cece77a4db1137c762
tuanpa87 <tuanpa87@users.noreply.github.com>
2021-01-06 23:29:59 +0700

    Writing and Generating Documentation

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,4 +1,8 @@
 defmodule Cards do
+  @moduledoc """
+    Provides methods for creating and handling a deck of cards
+  """
+
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five"]
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
@@ -33,8 +37,8 @@ defmodule Cards do
   end
 
   def create_hand(hand_size) do
-    Cards.create_deck
-    |> Cards.shuffle
+    Cards.create_deck()
+    |> Cards.shuffle()
     |> Cards.deal(hand_size)
   end
 end
