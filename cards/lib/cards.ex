abbc14908710d72428bcf6657a1d7fbb5db3acc5
tuanpa87 <tuanpa87@users.noreply.github.com>
2021-01-06 21:35:14 +0700

    format code

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,21 +1,22 @@
 defmodule Cards do
-    def create_deck do
-        values = ["Ace", "Two", "Three", "Four", "Five"]
-        suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
 
-        for suit <- suits, value <- values do
-			"#{value} of #{suit}"
-		end
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
     end
+  end
 
-    def shuffle(deck) do
-        Enum.shuffle(deck)
-    end
-    def contains?(deck, card) do
-        Enum.member?(deck, card)
-	end
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
 
-	def deal(deck, hand_size) do
-		Enum.split(deck, hand_size)
-	end
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
 end
