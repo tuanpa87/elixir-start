e00e335b77a4402aa702632159e346606c56a779
tuanpa87 <tuanpa87@users.noreply.github.com>
2021-02-24 16:04:30 +0700

    Building the Comment Form

--- discuss/web/channels/comments_channel.ex
+++ discuss/web/channels/comments_channel.ex
@@ -9,7 +9,8 @@ defmodule Discuss.CommentsChannel do
     {:ok, %{}, socket}
   end
 
-  def handle_in(name, message, socket) do
+  def handle_in(name, %{"content" => content}, socket) do
+
     {:reply, :ok, socket}
   end
 end
